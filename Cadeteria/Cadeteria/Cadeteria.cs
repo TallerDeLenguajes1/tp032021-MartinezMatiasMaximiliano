74b57a9e29e9f7ac2adefc6b8baee923072261eb
matias maximiliano martinez <matumartinez9@hotmail.com>
2021-09-01 19:54:41 -0300

    Clases y algunos metodos

---
+++ Cadeteria/Cadeteria/Cadeteria.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cadeteria
+{
+    class Cadeteria
+    {
+        private string nombre;
+        private List<Cadete> listaCadetes = new List<Cadete>();
+
+        public string Nombre { get => nombre; set => nombre = value; }
+
+        public Cadeteria(string _Nombre)
+        {
+            this.nombre = _Nombre;
+        }
+
+        public double calcularSalario(string _NombreCadete)
+        {
+            double salario = 0;
+            return salario;
+        }
+    }
+}
---
+++ Cadeteria/Cadeteria/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Cadeteria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Pedido nuevo = new Pedido("pizza",Pedido.EnumEstado.Entregado , "matias", "jc 364", "43322342");
+
+            Cadete nuevoCadete = new Cadete();
+            
+        }
+    }
+}
